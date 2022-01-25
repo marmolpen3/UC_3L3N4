22e1b8adb7a8b4d1152a7f946c04377dd9407f5d
marmolpen3 <marmolpen3@alum.us.es>
2022-01-25 12:49:38 +0100

    conf: Add package to read key-value from .env

--- uc/uc/settings.py
+++ uc/uc/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ov#x7(3)(7uy0(w&4d025uh=dds-o)@b%%%o)$^dd^01$e^org'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
